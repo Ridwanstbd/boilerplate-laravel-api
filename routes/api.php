d87105a82f08de8efa4f9b4f5bf143275dcf2046
ridwanstbd <ridwansetiobudi77@gmail.com>
2025-05-22 11:30:46 +0700

    chore: build up boilerplate

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,6 @@
 <?php
 
-use App\Http\Controllers\Feature\CostComponentController;
-use App\Http\Controllers\Feature\ExpenseCategoryController;
-use App\Http\Controllers\Feature\HppController;
-use App\Http\Controllers\Feature\OperationalExpenseController;
-use App\Http\Controllers\Feature\PriceSchemeController;
-use App\Http\Controllers\Feature\SalesRecordController;
-use App\Http\Controllers\Feature\StatsController;
 use App\Http\Controllers\JWTAuthController;
-use App\Http\Controllers\User\ProductController;
 use App\Http\Middleware\JWTMiddleware;
 use Illuminate\Support\Facades\Route;
 
@@ -21,12 +13,4 @@ Route::middleware(JWTMiddleware::class)->group(function(){
     Route::post('logout',[JWTAuthController::class,'logout']);
     Route::post('refresh',[JWTAuthController::class,'refresh']);
     Route::get('me',[JWTAuthController::class,'me']);
-    Route::get('stats',[StatsController::class,'stats']);
-    Route::apiResource('expense-categories', ExpenseCategoryController::class);
-    Route::apiResource('operational-expenses', OperationalExpenseController::class);    
-    Route::apiResource('cost-components',CostComponentController::class);
-    Route::apiResource('hpp',HppController::class);
-    Route::apiResource('sales',SalesRecordController::class);
-    Route::apiResource('price-schemes',PriceSchemeController::class);
-    Route::apiResource('products',ProductController::class);
 });
